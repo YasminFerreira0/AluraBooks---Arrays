76da2062bdcc791c0e3c5d3de5f07c4a488801c6
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-08-04 00:42:46 -0400

    .

---
+++ app/main.js
@@ -0,0 +1,10 @@
+let livros = [];
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+getBuscaLivros();
+
+async function getBuscaLivros (){
+    const res = await fetch(endpointDaAPI);
+    livros = await res.json();
+    console.table(livros);
+}
