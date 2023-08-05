6d3dee647c5b9fecd0a580fbc7ae24441d35a2eb
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-08-05 17:14:41 -0400

    Update metodoMap.js

--- app/metodoMap.js
+++ app/metodoMap.js
@@ -1,7 +1,7 @@
 function aplicarDesconto(livros) {
     const desconto = 0.3;
     livrosComDesconto = livros.map(livro => {
-        return{...livros, preco: livro.preco - (livro.preco * desconto)};
+        return{...livro, preco: livro.preco - (livro.preco * desconto)};
     });
     return livrosComDesconto;
 }
