f53647f78a8a1041d1c052e42fe6ffe56c5bfed3
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-08-10 16:26:53 -0400

    ..

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,9 +5,9 @@ const botoes = document.querySelectorAll('.btn');
 
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 
-function filtrarLivros (){
+function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
-    let livrosFiltrados = livros.filter(livros => livros.categoria == categoria);
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
     exibirOsLivrosNaTela(livrosFiltrados);
 }
