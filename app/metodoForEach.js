8da63861f0f9cefb60e259bafb9fb4bfd318a176
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-08-23 15:53:48 -0400

    metodo sort

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,9 +3,12 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
+        //let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
+
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+            <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
             <h2 class="livro__titulo">
                 ${livro.titulo}
             </h2>
@@ -19,3 +22,13 @@ function exibirOsLivrosNaTela(listaDeLivros) {
     });
     
 }
+
+/*function verificarDisponibilidadeDoLivro(livro) {
+    if (livro.quantidade > 0){
+        return 'livro__imagens'
+    }else{
+        return 'livros__imagens indisponivel'
+    }
+    
+}
+*/
